f4ae21898bed281a7bb9d74928e0eff4199830e1
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-01-11 10:45:48 -0600

    i think blocks are working @five upload textures pls

--- src/main/java/com/bagel/rosewood/Rosewood.java
+++ src/main/java/com/bagel/rosewood/Rosewood.java
@@ -6,17 +6,13 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.google.common.collect.Maps;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("rosewood")
 public class Rosewood
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    //private static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "rosewood";
 
     public Rosewood() {
