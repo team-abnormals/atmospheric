ab1b08b3ee9f1653b3ac53f8dd22d6c6d74e0d57
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-03 19:31:13 -0600

    Passion Vine Bundle changes (tree still wip)

--- src/main/java/com/bagel/rosewood/common/blocks/PassionVineBundleBlock.java
+++ src/main/java/com/bagel/rosewood/common/blocks/PassionVineBundleBlock.java
@@ -1,19 +1,21 @@
 package com.bagel.rosewood.common.blocks;
 
+import javax.annotation.Nullable;
+
 import com.bagel.rosewood.core.registry.RosewoodBlocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
 
 public class PassionVineBundleBlock extends Block {
@@ -28,42 +30,42 @@ public class PassionVineBundleBlock extends Block {
 	
 	@SuppressWarnings("deprecation")
 	@Override
-	public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-		
-		
-		BlockPos nextPos = pos.offset(Direction.DOWN);
-		Block nextBlock = worldIn.getBlockState(nextPos).getBlock();
-		int counter = 9;
-		
-  	    worldIn.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 1.0F, 0.8F + worldIn.rand.nextFloat() * 0.4F);
-
-		if (!worldIn.getBlockState(pos.offset(player.getHorizontalFacing())).getBlock().isAir(worldIn.getBlockState(pos.offset(player.getHorizontalFacing())))) {
-			BlockState vine = RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, player.getHorizontalFacing().getOpposite());
-			worldIn.setBlockState(pos, vine);
-			counter = 8;
-			while (counter > 0) {
-				if (nextBlock.isAir(worldIn.getBlockState(nextPos))) {
-					worldIn.setBlockState(nextPos, vine);
-					counter = counter - 1;
-					nextPos = nextPos.offset(Direction.DOWN);
-					nextBlock = worldIn.getBlockState(nextPos).getBlock();
-				} else {
-					break;
+	public void harvestBlock(World worldIn, PlayerEntity player, BlockPos pos, BlockState state, @Nullable TileEntity te, ItemStack stack) {
+	      super.harvestBlock(worldIn, player, pos, state, te, stack);
+	      if (EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, stack) != 0 || stack.getItem() == Items.SHEARS) {
+	        worldIn.removeBlock(pos, false);
+	        return;
+	      }
+	      
+	      BlockPos nextPos = pos.offset(Direction.DOWN);
+			Block nextBlock = worldIn.getBlockState(nextPos).getBlock();
+			int counter = 9;
+			
+			if (!worldIn.getBlockState(pos.offset(player.getHorizontalFacing())).getBlock().isAir(worldIn.getBlockState(pos.offset(player.getHorizontalFacing())))) {
+				BlockState vine = RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, player.getHorizontalFacing().getOpposite());
+				worldIn.setBlockState(pos, vine);
+				counter = 8;
+				while (counter > 0) {
+					if (nextBlock.isAir(worldIn.getBlockState(nextPos))) {
+						worldIn.setBlockState(nextPos, vine);
+						counter = counter - 1;
+						nextPos = nextPos.offset(Direction.DOWN);
+						nextBlock = worldIn.getBlockState(nextPos).getBlock();
+					} else {
+						break;
+					}
+				}
+				if (player.abilities.isCreativeMode == false) {
+					spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), counter));
+				}
+				
+			} else {
+				worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+				if (player.abilities.isCreativeMode == false) {
+					spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), 9));
 				}
 			}
-			if (player.abilities.isCreativeMode == false) {
-				spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), counter));
-			}
-			
-		} else {
-			worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
-			if (player.abilities.isCreativeMode == false) {
-				spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), 9));
-			}
-		}
-		return true;	
-	
-	}
+	      }
 }
 
 //if (PassionVineBlock.canAttachTo(RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, playerDirection.getOpposite()), iworldreader, pos.offset(Direction.DOWN))) {
