12d4d9c24eed0640d65bbf1d24ac1e455c27df98
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-03 22:58:37 -0600

    Minor fixes
    - Fixed Rosewood Bookshelf
    - Passion Vine Bundle now randomly prevents damage
    - Passion vine growth can now be stunted by blaze powder and resumed by glowstone dust

--- src/main/java/com/bagel/rosewood/common/blocks/PassionVineBlock.java
+++ src/main/java/com/bagel/rosewood/common/blocks/PassionVineBlock.java
@@ -18,6 +18,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.IntegerProperty;
@@ -40,6 +41,7 @@ import net.minecraft.world.World;
 public class PassionVineBlock extends Block implements IGrowable {
 	public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 	public static final IntegerProperty AGE = StateUtils.AGE_0_4; 
+	public static final BooleanProperty GROWABLE = StateUtils.GROWABLE; 
 	public static final EnumProperty<PassionVineAttachment> ATTACHMENT = StateUtils.PASSION_VINE_ATTACHMENT; 
 	   
 	protected static final VoxelShape EAST_AABB = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 1.0D, 16.0D, 16.0D);
@@ -59,7 +61,6 @@ public class PassionVineBlock extends Block implements IGrowable {
 		    	default:
 		    		return EAST_AABB;    		
 		}
-		
 	}
 	
 	public PassionVineBlock(Properties properties) {
@@ -67,46 +68,41 @@ public class PassionVineBlock extends Block implements IGrowable {
 	}
 	
 	@Override public boolean isLadder(BlockState state, IWorldReader world, BlockPos pos, net.minecraft.entity.LivingEntity entity) { return true; }
-	/*public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
-		for(LivingEntity entities : worldIn.getEntitiesWithinAABB(LivingEntity.class, state.getShape(worldIn, pos).getBoundingBox())) {
-			entities.setMotionMultiplier(state, new Vec3d(1.0D, (double)0.05F, 1.0D));
-		}
-	}*/
 	
 	@SuppressWarnings("deprecation")
 	public void tick(BlockState state, World worldIn, BlockPos pos, Random random) {
-	      super.tick(state, worldIn, pos, random);
+		super.tick(state, worldIn, pos, random);
+		determineState(state, worldIn, pos);
+	    int i = state.get(AGE);
+	    
+	    Boolean light = worldIn.getLightSubtracted(pos.up(), 0) >= 5;
+	    Boolean notOld = i < 4;
+	    Boolean canFlower = i >= 1;
+	    Boolean canFruit = notOld && light;
+	    Boolean randomNum = worldIn.rand.nextInt(2) == 1;
 	      
-	      determineState(state, worldIn, pos);
-	      Direction direction = state.get(FACING);
-	      BlockState hanging = worldIn.getBlockState(pos.offset(direction.getOpposite()));
-	      int i = state.get(AGE);
-	      
-	      Boolean light = worldIn.getLightSubtracted(pos.up(), 0) >= 5;
-	      Boolean notOld = i < 4;
-	      Boolean canFlower = i >= 1;
-	      Boolean canFruit = notOld && light;
-	      Boolean randomNum = worldIn.rand.nextInt(2) == 1;
-	      
-	      if (hanging.getBlock() != Blocks.MAGMA_BLOCK) {
-	    	  
-	    	  if (canFlower) {
-	    		  if (canFruit) {
-	    			  if (randomNum) { attemptGrowFruit(state, worldIn, pos, random); 
-	    			  } else { attemptGrowDown(state, worldIn, pos, random); }  
-	    		  } else {
-	    			  attemptGrowDown(state, worldIn, pos, random);
-	    		  }
-	    	  } else {
-	    		  if (canFruit) { attemptGrowFruit(state, worldIn, pos, random); }
-    		  }
-	      }
-	}
+	    if (state.get(GROWABLE)) {
+	    	if (canFlower) {
+	    		if (canFruit) {
+	    			if (randomNum) { 
+	    				attemptGrowFruit(state, worldIn, pos, random); 
+	    				} else { 
+	    					attemptGrowDown(state, worldIn, pos, random); 
+	    				}  
+	    			} else { 
+	    				attemptGrowDown(state, worldIn, pos, random);	
+	    			}
+	    		} else {
+	    			if (canFruit) { 
+	    				attemptGrowFruit(state, worldIn, pos, random); 
+	    			}	
+	    		}
+	    	}
+	    }
 	
 	public void determineState(BlockState state, World world, BlockPos pos) {
 		BlockState below = world.getBlockState(pos.down());
 		BlockState above = world.getBlockState(pos.up());
-		
 		Boolean vineAbove = (above.getBlock() == state.getBlock());
 		Boolean vineBelow = (below.getBlock() == state.getBlock());		
 		
@@ -115,13 +111,15 @@ public class PassionVineBlock extends Block implements IGrowable {
 			if (vineFacingAbove) {
 				if (vineBelow) { world.setBlockState(pos, state.with(ATTACHMENT, PassionVineAttachment.MIDDLE)); } 
 				else { world.setBlockState(pos, state.with(ATTACHMENT, PassionVineAttachment.BOTTOM)); }
-			}
+			}	
 		} else if (vineBelow) {
 			Boolean vineFacingBelow = below.getBlockState().get(FACING) == state.get(FACING);
 			if (vineFacingBelow) {
-				 { world.setBlockState(pos, state.with(ATTACHMENT, PassionVineAttachment.TOP)); } 
+				world.setBlockState(pos, state.with(ATTACHMENT, PassionVineAttachment.TOP));  
 			}
-		} else { world.setBlockState(pos, state.with(ATTACHMENT, PassionVineAttachment.NONE)); }
+		} else { 
+			world.setBlockState(pos, state.with(ATTACHMENT, PassionVineAttachment.NONE)); 	
+		}	
 	}
 	
 	public void attemptGrowFruit(BlockState state, World worldIn, BlockPos pos, Random random) {
@@ -129,25 +127,25 @@ public class PassionVineBlock extends Block implements IGrowable {
 		Direction direction = state.get(FACING);
 		BlockState hanging = worldIn.getBlockState(pos.offset(direction.getOpposite()));
 		if (hanging.getBlock().isIn(BlockTags.LOGS) || hanging.getBlock().isIn(BlockTags.LEAVES)) {
-			  if (i < 4 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt(7) == 0)) {
-				  worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(i + 1)), 2);
-				  net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
-			  }
-		  } else {
-			  if (i < 1 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt(7) == 0)) {
-				  worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(i + 1)), 2);
-				  net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
-			  }
-		  }
+			if (i < 4 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt(7) == 0)) {
+				worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(i + 1)), 2);
+				net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);	
+			}	
+		} else {
+			if (i < 1 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt(7) == 0)) {
+				worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(i + 1)), 2);
+				net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);	
+			}	
+		}
 	}
 	
 	public void attemptGrowDown(BlockState state, World worldIn, BlockPos pos, Random random) {
 		if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt(7) == 0)) {
-			  BlockState below = worldIn.getBlockState(pos.down());
-			  if (below.getBlock() == Blocks.AIR) {
-				  worldIn.setBlockState(pos.down(), RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(AGE, Integer.valueOf(0)).with(FACING, state.get(FACING)));
-			  }
-		  }
+			BlockState below = worldIn.getBlockState(pos.down());
+			if (below.getBlock() == Blocks.AIR) {
+				worldIn.setBlockState(pos.down(), RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(AGE, Integer.valueOf(0)).with(FACING, state.get(FACING)));	
+			}	
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -155,7 +153,19 @@ public class PassionVineBlock extends Block implements IGrowable {
 	    determineState(state, worldIn, pos);
 		int i = state.get(AGE);
 		boolean flag = i == 4;
-		if (!flag && player.getHeldItem(handIn).getItem() == Items.BONE_MEAL) {
+		if (player.getHeldItem(handIn).getItem() == Items.BLAZE_POWDER && state.get(GROWABLE)) {
+			worldIn.setBlockState(pos, state.with(GROWABLE, false));
+			if (!player.abilities.isCreativeMode) {
+				player.getHeldItem(handIn).shrink(1);
+			}
+			return true;
+		} else if (player.getHeldItem(handIn).getItem() == Items.GLOWSTONE_DUST && state.get(GROWABLE) == false) {
+			worldIn.setBlockState(pos, state.with(GROWABLE, true));
+			if (!player.abilities.isCreativeMode) {
+				player.getHeldItem(handIn).shrink(1);
+			}
+			return true;
+		} else if (!flag && player.getHeldItem(handIn).getItem() == Items.BONE_MEAL) {
 			return false;	
 		} else if (i == 4) {
 			spawnAsEntity(worldIn, pos, new ItemStack(RosewoodItems.PASSIONFRUIT.get(), 1 + worldIn.rand.nextInt(2) + worldIn.rand.nextInt(2) + worldIn.rand.nextInt(3)));
@@ -180,67 +190,64 @@ public class PassionVineBlock extends Block implements IGrowable {
 	}
 	
 	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(ATTACHMENT, AGE, FACING);
+		builder.add(ATTACHMENT, AGE, FACING, GROWABLE);
 	}
 	
 	public static boolean canAttachTo(IBlockReader block, BlockPos pos, Direction direction) {
-	      BlockState blockstate = block.getBlockState(pos);
-	      return !blockstate.canProvidePower() && blockstate.func_224755_d(block, pos, direction);
-	   }
+		BlockState blockstate = block.getBlockState(pos);
+	    return !blockstate.canProvidePower() && blockstate.func_224755_d(block, pos, direction);    
+	}
 
-	   public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
-	      Direction direction = state.get(FACING);
-	      return (PassionVineBlock.canAttachTo(worldIn, pos.offset(direction.getOpposite()), direction) 
-	    		  || (worldIn.getBlockState(pos.offset(Direction.UP)).getBlock() == RosewoodBlocks.PASSION_VINE.get() 
-	    		  && worldIn.getBlockState(pos.offset(Direction.UP)).get(FACING) == state.get(FACING)));
-	   }
+	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+		Direction direction = state.get(FACING);
+	    return (PassionVineBlock.canAttachTo(worldIn, pos.offset(direction.getOpposite()), direction) 
+	    		|| (worldIn.getBlockState(pos.offset(Direction.UP)).getBlock() == RosewoodBlocks.PASSION_VINE.get() 
+	    		&& worldIn.getBlockState(pos.offset(Direction.UP)).get(FACING) == state.get(FACING)));    
+	}
 	   
-	   @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-		   determineState(stateIn, worldIn.getWorld(), currentPos);
-		   if (!stateIn.isValidPosition(worldIn, currentPos)) {
-		         return Blocks.AIR.getDefaultState();
-		      } else {
-		         return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-		      }
-		   }
+		determineState(stateIn, worldIn.getWorld(), currentPos);
+		if (!stateIn.isValidPosition(worldIn, currentPos)) {
+			return Blocks.AIR.getDefaultState();
+		} else {
+			return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+		}
+	}
 	
 	@Nullable
-	   public BlockState getStateForPlacement(BlockItemUseContext context) {
-	      if (!context.replacingClickedOnBlock()) {
-	         BlockState blockstate = context.getWorld().getBlockState(context.getPos().offset(context.getFace().getOpposite()));
-	         if (blockstate.getBlock() == this && blockstate.get(FACING) == context.getFace()) {
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		if (!context.replacingClickedOnBlock()) {
+			BlockState blockstate = context.getWorld().getBlockState(context.getPos().offset(context.getFace().getOpposite()));
+	    	if (blockstate.getBlock() == this && blockstate.get(FACING) == context.getFace()) {
 	            return null;
-	         }
-	      }
+	        }
+	    }
+		
+		BlockState blockstate1 = this.getDefaultState();
+	    IWorldReader iworldreader = context.getWorld();
+	    BlockPos blockpos = context.getPos();
 
-	      BlockState blockstate1 = this.getDefaultState();
-	      IWorldReader iworldreader = context.getWorld();
-	      BlockPos blockpos = context.getPos();
-
-	      for(Direction direction : context.getNearestLookingDirections()) {
-	         if (direction.getAxis().isHorizontal()) {
-	            blockstate1 = blockstate1.with(FACING, direction.getOpposite());
+	    for(Direction direction : context.getNearestLookingDirections()) {
+	    	if (direction.getAxis().isHorizontal()) {
+	    		blockstate1 = blockstate1.with(FACING, direction.getOpposite());
 	            if (blockstate1.isValidPosition(iworldreader, blockpos)) {
-	               return blockstate1;
+	            	return blockstate1.with(GROWABLE, true);
 	            }
-	         }
-	      }
-
-	      return null;
-	   }
+	        }	
+	    }
+	    
+	    return null;    
+	}
 	
 	@Override
     public BlockRenderLayer getRenderLayer() {
-	    return BlockRenderLayer.CUTOUT;
+		return BlockRenderLayer.CUTOUT;
 	}
 	
 	public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
-		//Direction direction = state.get(FACING);
-		//BlockState hanging = worldIn.getBlockState(pos.offset(direction.getOpposite()));
-		return state.get(AGE) < 4;
-
-	   }
+		return state.get(AGE) < 4;	
+	}
 
 	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
 		return true;
@@ -252,13 +259,3 @@ public class PassionVineBlock extends Block implements IGrowable {
 	    worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(i)), 2);
 	}
 }
-
-/*
-if the vine is on a log or leaves
--> grow to stage 5
-
-if the vine is not
--> grow of stage 2
-
-if the 
-*/
--- src/main/java/com/bagel/rosewood/common/blocks/PassionVineBundleBlock.java
+++ src/main/java/com/bagel/rosewood/common/blocks/PassionVineBundleBlock.java
@@ -1,5 +1,7 @@
 package com.bagel.rosewood.common.blocks;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
 import com.bagel.rosewood.core.registry.RosewoodBlocks;
@@ -19,56 +21,54 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class PassionVineBundleBlock extends Block {
-
+	protected final Random rand = new Random();
+	
 	public PassionVineBundleBlock(Properties properties) {
 		super(properties);
 	}
 	
 	public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance) {
-		entityIn.fall(fallDistance, 0.2F);	
+		entityIn.fall(fallDistance, rand.nextFloat());	
 	}
 	
 	@SuppressWarnings("deprecation")
 	@Override
 	public void harvestBlock(World worldIn, PlayerEntity player, BlockPos pos, BlockState state, @Nullable TileEntity te, ItemStack stack) {
-	      super.harvestBlock(worldIn, player, pos, state, te, stack);
-	      if (EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, stack) != 0 || stack.getItem() == Items.SHEARS) {
-	        worldIn.removeBlock(pos, false);
-	        return;
-	      }
-	      
-	      BlockPos nextPos = pos.offset(Direction.DOWN);
-			Block nextBlock = worldIn.getBlockState(nextPos).getBlock();
-			int counter = 9;
-			
-			if (!worldIn.getBlockState(pos.offset(player.getHorizontalFacing())).getBlock().isAir(worldIn.getBlockState(pos.offset(player.getHorizontalFacing())))) {
-				BlockState vine = RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, player.getHorizontalFacing().getOpposite());
-				worldIn.setBlockState(pos, vine);
-				counter = 8;
-				while (counter > 0) {
-					if (nextBlock.isAir(worldIn.getBlockState(nextPos))) {
-						worldIn.setBlockState(nextPos, vine);
-						counter = counter - 1;
-						nextPos = nextPos.offset(Direction.DOWN);
-						nextBlock = worldIn.getBlockState(nextPos).getBlock();
-					} else {
-						break;
-					}
-				}
-				if (player.abilities.isCreativeMode == false) {
-					spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), counter));
-				}
-				
-			} else {
-				worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
-				if (player.abilities.isCreativeMode == false) {
-					spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), 9));
-				}
+		super.harvestBlock(worldIn, player, pos, state, te, stack);
+	    if (EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, stack) != 0 || stack.getItem() == Items.SHEARS) {
+	    	worldIn.removeBlock(pos, false);
+	    	return;	
+	    }
+
+	    BlockPos nextPos = pos.offset(Direction.DOWN);
+	    Block nextBlock = worldIn.getBlockState(nextPos).getBlock();
+	    //IBlockReader blockreader = (IBlockReader) state.getBlock();
+		int counter = 9;
+		if (!worldIn.getBlockState(pos.offset(player.getHorizontalFacing())).getBlock().isAir(worldIn.getBlockState(pos.offset(player.getHorizontalFacing()))) 
+				//|| PassionVineBlock.canAttachTo(blockreader, pos.offset(player.getHorizontalFacing()), player.getHorizontalFacing())
+				) {
+			BlockState vine = RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, player.getHorizontalFacing().getOpposite());
+			worldIn.setBlockState(pos, vine);
+			counter = 8;
+			while (counter > 0) {
+				if (nextBlock.isAir(worldIn.getBlockState(nextPos))) {
+					worldIn.setBlockState(nextPos, vine);
+					counter = counter - 1;
+					nextPos = nextPos.offset(Direction.DOWN);
+					nextBlock = worldIn.getBlockState(nextPos).getBlock();	
+				} else {
+					break;	
+				}	
 			}
-	      }
+			
+			if (player.abilities.isCreativeMode == false) {
+				spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), counter));	
+			}	
+		} else {
+			worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+			if (player.abilities.isCreativeMode == false) {
+				spawnAsEntity(worldIn, nextPos, new ItemStack(RosewoodBlocks.PASSION_VINE.get(), 9));	
+			}	
+		}	
+	}
 }
-
-//if (PassionVineBlock.canAttachTo(RosewoodBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, playerDirection.getOpposite()), iworldreader, pos.offset(Direction.DOWN))) {
-//IWorldReader iworldreader = worldIn.getWorld();
-
-//}
