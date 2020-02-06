1b9e345409359da7fae0f9882fe49d7e44da7031
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-06 17:27:47 -0600

    <@291118470559563776>

--- src/main/java/com/bagel/rosewood/core/registry/RosewoodItems.java
+++ src/main/java/com/bagel/rosewood/core/registry/RosewoodItems.java
@@ -7,6 +7,8 @@ import com.bagel.rosewood.core.util.RegistryUtils;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
+import net.minecraft.item.SoupItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,6 +19,6 @@ public class RosewoodItems {
 	public static RegistryObject<Item> PASSIONFRUIT = RegistryUtils.createItem("passionfruit", () -> new Item(new Item.Properties().food(RosewoodFoods.PASSIONFRUIT).group(ItemGroup.FOOD)));
 	public static RegistryObject<Item> SHIMMERING_PASSIONFRUIT = RegistryUtils.createItem("shimmering_passionfruit", () -> new Item(new Item.Properties().food(RosewoodFoods.SHIMMERING_PASSIONFRUIT).group(ItemGroup.FOOD)));
 	public static RegistryObject<Item> PASSIONFRUIT_TART = RegistryUtils.createItem("passionfruit_tart", () -> new Item(new Item.Properties().food(RosewoodFoods.PASSIONFRUIT_TART).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> PASSIONFRUIT_SORBET = RegistryUtils.createItem("passionfruit_sorbet", () -> new SoupItem(new Item.Properties().food(RosewoodFoods.PASSIONFRUIT_SORBET).containerItem(Items.BOWL).group(ItemGroup.FOOD)));
 	public static RegistryObject<Item> ROSEWOOD_BOAT = RegistryUtils.createItem("rosewood_boat", () -> new RosewoodBoatItem(RosewoodBoatEntity.Type.ROSEWOOD, new Item.Properties().group(ItemGroup.TRANSPORTATION)));
-
 }
