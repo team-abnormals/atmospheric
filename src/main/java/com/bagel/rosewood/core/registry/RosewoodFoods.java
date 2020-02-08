c398086c3024ac9bd137776cdb44c38120f10d8d
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-07 23:39:58 -0600

    Passionfruit is always edible

--- src/main/java/com/bagel/rosewood/core/registry/RosewoodFoods.java
+++ src/main/java/com/bagel/rosewood/core/registry/RosewoodFoods.java
@@ -16,8 +16,8 @@ public class RosewoodFoods {
 	public static Effect SPITTING = new SpittingEffect().setRegistryName("spitting");
     public static Effect GOLDEN_SPITTING = new GoldenSpittingEffect().setRegistryName("golden_spitting");
 	
-	public static Food PASSIONFRUIT = new Food.Builder().hunger(2).saturation(0.3F).effect(new EffectInstance(SPITTING, 140, 0, true, false), 1.0F).build();
-	public static Food SHIMMERING_PASSIONFRUIT = new Food.Builder().hunger(5).saturation(1.2F).effect(new EffectInstance(GOLDEN_SPITTING, 160, 0, true, false), 1.0F).build();
+	public static Food PASSIONFRUIT = new Food.Builder().hunger(2).saturation(0.3F).setAlwaysEdible().effect(new EffectInstance(SPITTING, 140, 0, true, false), 1.0F).build();
+	public static Food SHIMMERING_PASSIONFRUIT = new Food.Builder().hunger(5).saturation(1.2F).setAlwaysEdible().effect(new EffectInstance(GOLDEN_SPITTING, 160, 0, true, false), 1.0F).build();
 	public static Food PASSIONFRUIT_TART = new Food.Builder().hunger(3).saturation(0.6F).build();
 	public static Food PASSIONFRUIT_SORBET = new Food.Builder().hunger(15).saturation(0.5F).effect(new EffectInstance(Effects.SLOWNESS, 300, 4, false, false), 1.0F).build();
 }
