e8002217a1a0f0cf93a7bbcfda9012fd41ad1d0b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-14 21:30:46 -0600

    Added some more tags

--- src/main/java/com/bagel/atmospheric/common/block/YuccaSaplingBlock.java
+++ src/main/java/com/bagel/atmospheric/common/block/YuccaSaplingBlock.java
@@ -1,14 +1,12 @@
 package com.bagel.atmospheric.common.block;
 
-import com.bagel.atmospheric.core.registry.AtmosphericBlocks;
-
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.SaplingBlock;
 import net.minecraft.block.trees.Tree;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 
 public class YuccaSaplingBlock extends SaplingBlock implements net.minecraftforge.common.IPlantable {
 	public YuccaSaplingBlock(Tree tree, Properties properties) {
@@ -16,10 +14,8 @@ public class YuccaSaplingBlock extends SaplingBlock implements net.minecraftforg
     }
 	
 	@Override
-	protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
-		Block block = state.getBlock();
-	    return block == Blocks.GRASS_BLOCK || block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.PODZOL || block == Blocks.FARMLAND
-	    		|| block == Blocks.SAND || block == Blocks.RED_SAND || block == AtmosphericBlocks.ARID_SAND.get();      
+	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+		return worldIn.getBlockState(pos.down()).isIn(BlockTags.BAMBOO_PLANTABLE_ON);
 	}
 	
 	@Override
