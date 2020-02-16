a2a4d86facc0d4a60651a333b8d8a004eac02440
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-15 18:31:28 -0600

    Prepared Sapling Trades

--- src/main/java/com/bagel/atmospheric/core/registry/AtmosphericTrades.java
+++ src/main/java/com/bagel/atmospheric/core/registry/AtmosphericTrades.java
@@ -15,8 +15,12 @@ public class AtmosphericTrades {
 	@SubscribeEvent
 	public static void onWandererTradesEvent(WandererTradesEvent event) {
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.MONKEY_BRUSH.get(), 1, 1, 6, 1));
-		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.ROSEWOOD_SAPLING.get(), 5, 1, 8, 1));
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.PASSION_VINE.get(), 4, 1, 8, 1));
+		
+		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.ROSEWOOD_SAPLING.get(), 5, 1, 8, 1));
+		//event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.YUCCA_SAPLING.get(), 5, 1, 8, 1));
+		//event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.KOUSA_SAPLING.get(), 5, 1, 8, 1));
+		//event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.ASPEN_SAPLING.get(), 5, 1, 8, 1));
 	}
 	
 	@SubscribeEvent
