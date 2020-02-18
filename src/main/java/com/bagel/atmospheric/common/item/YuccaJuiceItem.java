fdeb68cc278fd5ed551b12a9dabfa12fc9acb4db
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-18 16:18:42 -0600

    Some item & block models + lang

--- src/main/java/com/bagel/atmospheric/common/item/YuccaJuiceItem.java
+++ src/main/java/com/bagel/atmospheric/common/item/YuccaJuiceItem.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.UseAction;
 import net.minecraft.world.World;
 
 public class YuccaJuiceItem extends Item {
@@ -14,5 +15,10 @@ public class YuccaJuiceItem extends Item {
    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
       super.onItemUseFinish(stack, worldIn, entityLiving);
       return new ItemStack(Items.GLASS_BOTTLE);
+   }
+   
+   @Override
+	public UseAction getUseAction(ItemStack stack) {
+       return UseAction.DRINK;
    }
 }
