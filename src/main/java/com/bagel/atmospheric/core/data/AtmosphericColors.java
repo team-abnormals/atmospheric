f46ae90be9587dfb448367e8390485ad788193ec
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-22 11:52:47 -0600

    Fixed leaf carpet colors when missing quark

--- src/main/java/com/bagel/atmospheric/core/data/AtmosphericColors.java
+++ src/main/java/com/bagel/atmospheric/core/data/AtmosphericColors.java
@@ -7,7 +7,6 @@ import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.world.FoliageColors;
 import net.minecraft.world.biome.BiomeColors;
-import net.minecraftforge.fml.ModList;
 
 public class AtmosphericColors
 {
@@ -20,11 +19,9 @@ public class AtmosphericColors
         itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.ROSEWOOD_LEAVES.get());
         itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.YUCCA_LEAVES.get());
         
-        if (ModList.get().isLoaded("quark")) {
-            blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.ROSEWOOD_LEAF_CARPET.get());
-            blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.YUCCA_LEAF_CARPET.get());
-        	itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.ROSEWOOD_LEAF_CARPET.get());
-        	itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.YUCCA_LEAF_CARPET.get());
-        }
+        blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.ROSEWOOD_LEAF_CARPET.get());
+        blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.YUCCA_LEAF_CARPET.get());
+        itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.ROSEWOOD_LEAF_CARPET.get());
+        itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), AtmosphericBlocks.YUCCA_LEAF_CARPET.get());
     }
 }
