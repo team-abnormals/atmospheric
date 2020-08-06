35ed57c3b1c3331044cc79ccce4d060c3c093655
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-06 15:06:17 -0500

    Dunes Villager

--- src/main/java/com/bagel/atmospheric/core/other/AtmosphericVillagers.java
+++ src/main/java/com/bagel/atmospheric/core/other/AtmosphericVillagers.java
@@ -1,24 +1,27 @@
 package com.bagel.atmospheric.core.other;
 
+import com.bagel.atmospheric.core.Atmospheric;
 import com.bagel.atmospheric.core.registry.AtmosphericBiomes;
 
+import net.minecraft.entity.merchant.villager.VillagerTrades;
 import net.minecraft.entity.villager.IVillagerType;
 
 public class AtmosphericVillagers {
-    //public static final IVillagerType DUNES = IVillagerType.register(Atmospheric.MODID + ":dunes");
 
     public static void setupVillagerTypes() {
-
+        VillagerTrades.VILLAGER_DEFAULT_TRADES.isEmpty();
+        IVillagerType dunes = IVillagerType.register(Atmospheric.MODID + ":dunes");
+        
         IVillagerType.BY_BIOME.put(AtmosphericBiomes.RAINFOREST.get(), IVillagerType.JUNGLE);
         IVillagerType.BY_BIOME.put(AtmosphericBiomes.RAINFOREST_MOUNTAINS.get(), IVillagerType.JUNGLE);
         IVillagerType.BY_BIOME.put(AtmosphericBiomes.RAINFOREST_PLATEAU.get(), IVillagerType.JUNGLE);
         IVillagerType.BY_BIOME.put(AtmosphericBiomes.SPARSE_RAINFOREST_PLATEAU.get(), IVillagerType.JUNGLE);
 
-        IVillagerType.BY_BIOME.put(AtmosphericBiomes.DUNES.get(), IVillagerType.DESERT);
-        IVillagerType.BY_BIOME.put(AtmosphericBiomes.DUNES_HILLS.get(), IVillagerType.DESERT);
-        IVillagerType.BY_BIOME.put(AtmosphericBiomes.FLOURISHING_DUNES.get(), IVillagerType.DESERT);
-        IVillagerType.BY_BIOME.put(AtmosphericBiomes.ROCKY_DUNES.get(), IVillagerType.DESERT);
-        IVillagerType.BY_BIOME.put(AtmosphericBiomes.ROCKY_DUNES_HILLS.get(), IVillagerType.DESERT);
-        IVillagerType.BY_BIOME.put(AtmosphericBiomes.PETRIFIED_DUNES.get(), IVillagerType.DESERT);
+        IVillagerType.BY_BIOME.put(AtmosphericBiomes.DUNES.get(), dunes);
+        IVillagerType.BY_BIOME.put(AtmosphericBiomes.DUNES_HILLS.get(), dunes);
+        IVillagerType.BY_BIOME.put(AtmosphericBiomes.FLOURISHING_DUNES.get(), dunes);
+        IVillagerType.BY_BIOME.put(AtmosphericBiomes.ROCKY_DUNES.get(), dunes);
+        IVillagerType.BY_BIOME.put(AtmosphericBiomes.ROCKY_DUNES_HILLS.get(), dunes);
+        IVillagerType.BY_BIOME.put(AtmosphericBiomes.PETRIFIED_DUNES.get(), dunes);
     }
 }
