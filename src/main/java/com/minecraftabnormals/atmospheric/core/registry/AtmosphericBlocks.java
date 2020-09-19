474f9bb9b13ca6b825e2967e42c878ecaba7977a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-18 21:59:19 -0500

    Aloe Bundles are mined faster with hoes; Closes #52

--- src/main/java/com/minecraftabnormals/atmospheric/core/registry/AtmosphericBlocks.java
+++ src/main/java/com/minecraftabnormals/atmospheric/core/registry/AtmosphericBlocks.java
@@ -226,7 +226,7 @@ public class AtmosphericBlocks {
 	
 	public static final RegistryObject<Block> ALOE_VERA	 			= HELPER.createBlockNoItem("aloe_vera", 	 () -> new AloeVeraBlock(AtmosphericProperties.ALOE_VERA));
 	public static final RegistryObject<Block> TALL_ALOE_VERA	 	= HELPER.createBlockNoItem("tall_aloe_vera", () -> new AloeVeraTallBlock(AtmosphericProperties.ALOE_VERA));
-	public static final RegistryObject<Block> ALOE_BUNDLE 			= HELPER.createBlock("aloe_bundle", 	 () -> new RotatedPillarBlock(Block.Properties.from(Blocks.DRIED_KELP_BLOCK)), ItemGroup.DECORATIONS);
+	public static final RegistryObject<Block> ALOE_BUNDLE 			= HELPER.createBlock("aloe_bundle", 	 () -> new RotatedPillarBlock(Block.Properties.from(Blocks.DRIED_KELP_BLOCK).harvestTool(ToolType.HOE)), ItemGroup.DECORATIONS);
 	public static final RegistryObject<Block> ALOE_GEL_BLOCK		= HELPER.createBlock("aloe_gel_block", 	 () -> new AloeGelBlock(Block.Properties.from(Blocks.SLIME_BLOCK)), ItemGroup.DECORATIONS);
 	public static final RegistryObject<Block> BARREL_CACTUS 		= HELPER.createBlock("barrel_cactus", 	 () -> new BarrelCactusBlock(Block.Properties.from(Blocks.CACTUS).tickRandomly()), ItemGroup.DECORATIONS);
 	public static final RegistryObject<Block> POTTED_BARREL_CACTUS	= HELPER.createBlockNoItem("potted_barrel_cactus",	() -> new FlowerPotBlock(BARREL_CACTUS.get(), Block.Properties.from(Blocks.POTTED_CACTUS)));
