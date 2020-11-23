2d1db418f354c51d0627541c937296a6d7f30322
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-23 13:56:23 -0600

    Changed Passion vine generation a bit

--- src/main/java/com/minecraftabnormals/atmospheric/common/world/gen/feature/PassionVineFeature.java
+++ src/main/java/com/minecraftabnormals/atmospheric/common/world/gen/feature/PassionVineFeature.java
@@ -1,11 +1,9 @@
 package com.minecraftabnormals.atmospheric.common.world.gen.feature;
 
-import java.util.Random;
-
 import com.minecraftabnormals.atmospheric.common.block.PassionVineBlock;
+import com.minecraftabnormals.atmospheric.core.other.AtmosphericTags;
 import com.minecraftabnormals.atmospheric.core.registry.AtmosphericBlocks;
 import com.mojang.serialization.Codec;
-
 import net.minecraft.block.BlockState;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -15,36 +13,44 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.StructureManager;
 
+import java.util.Random;
+
 public class PassionVineFeature extends Feature<NoFeatureConfig> {
-    public PassionVineFeature(Codec<NoFeatureConfig> p_i49876_1_) {
-        super(p_i49876_1_);
-    }
+	public PassionVineFeature(Codec<NoFeatureConfig> p_i49876_1_) {
+		super(p_i49876_1_);
+	}
 
-    @Override
-    public boolean func_230362_a_(ISeedReader worldIn, StructureManager manager, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config) {
-        int i = 0;
-        for (int j = 0; j < 400; ++j) {
+	@Override
+	public boolean func_230362_a_(ISeedReader worldIn, StructureManager manager, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config) {
+		int i = 0;
+		for (int j = 0; j < 400; ++j) {
+			Direction direction = Direction.Plane.HORIZONTAL.random(rand);
+			BlockState blockstate = AtmosphericBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, direction);
+			BlockPos blockpos = pos.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
 
-            Direction direction = Direction.Plane.HORIZONTAL.random(rand);
-            BlockState blockstate = AtmosphericBlocks.PASSION_VINE.get().getDefaultState().with(PassionVineBlock.FACING, direction).with(PassionVineBlock.AGE, rand.nextInt(2));
-            BlockPos blockpos = pos.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
+			int lengthA = 3 + rand.nextInt(2) + rand.nextInt(2) + rand.nextInt(2) - rand.nextInt(3);
 
-            int lengthA = 3 + rand.nextInt(2) + rand.nextInt(2) + rand.nextInt(2) - rand.nextInt(3);
+			if (worldIn.isAirBlock(blockpos) && blockpos.getY() > 50 && blockpos.getY() < 255 && blockstate.isValidPosition(worldIn, blockpos)) {
+				worldIn.setBlockState(blockpos, getVineState(worldIn, blockstate, blockpos, rand), 6);
+				for (int length = 0; length < lengthA; ++length) {
+					blockpos = blockpos.down();
+					if (worldIn.isAirBlock(blockpos) && blockpos.getY() < 255 && blockstate.isValidPosition(worldIn, blockpos)) {
+						worldIn.setBlockState(blockpos, getVineState(worldIn, blockstate, blockpos, rand), 6);
+					} else {
+						break;
+					}
+				}
+				++i;
+			}
+		}
+		return i > 0;
+	}
 
-            if (worldIn.isAirBlock(blockpos) && blockpos.getY() > 50 && blockpos.getY() < 255 && blockstate.isValidPosition(worldIn, blockpos)) {
-                worldIn.setBlockState(blockpos, blockstate, 6);
-                for (int length = 0; length < lengthA; ++length) {
-                    blockpos = blockpos.down();
-                    if (worldIn.isAirBlock(blockpos) && blockpos.getY() < 255 && blockstate.isValidPosition(worldIn, blockpos)) {
-                        worldIn.setBlockState(blockpos, blockstate.with(PassionVineBlock.AGE, rand.nextInt(2)), 6);
-                    } else {
-                        break;
-                    }
-                }
-                ++i;
-            }
-        }
-        return i > 0;
-
-    }
+	private static BlockState getVineState(ISeedReader world, BlockState state, BlockPos pos, Random rand) {
+		if (world.getBlockState(pos.offset(state.get(PassionVineBlock.FACING).getOpposite())).isIn(AtmosphericTags.PASSION_VINE_GROWABLE_ON)) {
+			return state.with(PassionVineBlock.AGE, 4);
+		} else {
+			return state.with(PassionVineBlock.AGE, 2);
+		}
+	}
 }
