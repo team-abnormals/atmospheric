d8c2b97f93b52323724d0d2ac9d0b99591ba119e
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-30 03:59:05 -0600

    Fixed some trades

--- src/main/java/com/minecraftabnormals/atmospheric/core/other/AtmosphericTrades.java
+++ src/main/java/com/minecraftabnormals/atmospheric/core/other/AtmosphericTrades.java
@@ -31,18 +31,16 @@ public class AtmosphericTrades {
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.ROSEWOOD_SAPLING.get(), 5, 1, 8, 1));
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.YUCCA_SAPLING.get(), 5, 1, 8, 1));
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.MORADO_SAPLING.get(), 5, 1, 8, 1));
-		//event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.KOUSA_SAPLING.get(), 5, 1, 8, 1));
-		//event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericBlocks.ASPEN_SAPLING.get(), 5, 1, 8, 1));
 	}
 	
 	@SubscribeEvent
 	public static void onVillagerTradesEvent(VillagerTradesEvent event) {
 		if(event.getType() == VillagerProfession.FARMER) {
 			event.getTrades().get(3).add(new TradeUtils.EmeraldsForItemsTrade(AtmosphericItems.PASSIONFRUIT.get(), 8, 1, 12, 10));
-			event.getTrades().get(3).add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericItems.PASSIONFRUIT_TART.get(), 16, 4, 4, 15));	
+			event.getTrades().get(2).add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericItems.PASSIONFRUIT_TART.get(), 1, 4, 12, 5));
 			event.getTrades().get(2).add(new TradeUtils.EmeraldsForItemsTrade(AtmosphericItems.ALOE_LEAVES.get(), 4, 1, 16, 15));
 			event.getTrades().get(2).add(new TradeUtils.EmeraldsForItemsTrade(AtmosphericItems.YUCCA_FRUIT.get(), 3, 1, 12, 10));
-			event.getTrades().get(2).add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericItems.YUCCA_GATEAU.get(), 12, 1, 1, 10));	
+			event.getTrades().get(4).add(new TradeUtils.ItemsForEmeraldsTrade(AtmosphericItems.YUCCA_GATEAU.get(), 3, 1, 12, 15));
 		}
     }
 }
