7a2b04e969993ce681145d67dd72103b6a10407a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-02-26 13:46:06 -0600

    Reformatted localization file

--- src/main/java/com/minecraftabnormals/atmospheric/core/other/AtmosphericDamageSources.java
+++ src/main/java/com/minecraftabnormals/atmospheric/core/other/AtmosphericDamageSources.java
@@ -1,5 +1,6 @@
 package com.minecraftabnormals.atmospheric.core.other;
 
+import com.minecraftabnormals.atmospheric.core.Atmospheric;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.IndirectEntityDamageSource;
@@ -7,14 +8,18 @@ import net.minecraft.util.IndirectEntityDamageSource;
 import javax.annotation.Nullable;
 
 public class AtmosphericDamageSources {
-	public static final DamageSource YUCCA_SAPLING = new DamageSource("yuccaSapling");
-	public static final DamageSource YUCCA_FLOWER = new DamageSource("yuccaFlower");
-	public static final DamageSource YUCCA_BRANCH = new DamageSource("yuccaBranch");
-	public static final DamageSource YUCCA_LEAVES = new DamageSource("yuccaLeaves");
-	public static final DamageSource BARREL_CACTUS = new DamageSource("barrelCactus");
-	public static final DamageSource ALOE_LEAVES = new DamageSource("aloeLeaves");
+	public static final DamageSource YUCCA_SAPLING = createDamageSource("yuccaSapling");
+	public static final DamageSource YUCCA_FLOWER = createDamageSource("yuccaFlower");
+	public static final DamageSource YUCCA_BRANCH = createDamageSource("yuccaBranch");
+	public static final DamageSource YUCCA_LEAVES = createDamageSource("yuccaLeaves");
+	public static final DamageSource BARREL_CACTUS = createDamageSource("barrelCactus");
+	public static final DamageSource ALOE_LEAVES = createDamageSource("aloeLeaves");
 
 	public static DamageSource causePassionfruitSeedDamage(Entity source, @Nullable Entity indirectEntityIn) {
-		return (new IndirectEntityDamageSource("passionfruitSeed", source, indirectEntityIn)).setProjectile();
+		return (new IndirectEntityDamageSource(Atmospheric.MOD_ID + ":passionfruitSeed", source, indirectEntityIn)).setProjectile();
+	}
+
+	private static DamageSource createDamageSource(String name) {
+		return new DamageSource(Atmospheric.MOD_ID + "." + name);
 	}
 }
