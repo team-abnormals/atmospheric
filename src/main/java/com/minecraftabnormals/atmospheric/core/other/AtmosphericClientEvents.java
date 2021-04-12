8508de411656f9266423e35803a0c9be3d5d0eca
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-04-11 20:24:06 -0500

    Update AtmosphericClientEvents.java

--- src/main/java/com/minecraftabnormals/atmospheric/core/other/AtmosphericClientEvents.java
+++ src/main/java/com/minecraftabnormals/atmospheric/core/other/AtmosphericClientEvents.java
@@ -2,9 +2,9 @@ package com.minecraftabnormals.atmospheric.core.other;
 
 import com.minecraftabnormals.atmospheric.core.Atmospheric;
 import com.minecraftabnormals.atmospheric.core.AtmosphericConfig;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
@@ -19,8 +19,11 @@ public class AtmosphericClientEvents {
 	public static void onItemTooltip(ItemTooltipEvent event) {
 		Item item = event.getItemStack().getItem();
 		ResourceLocation name = item.getRegistryName();
+		PlayerEntity player = event.getPlayer();
+		if (name == null || player == null)
+			return;
 
-		if (name != null && AtmosphericConfig.CLIENT.showUnobtainableDescription.get() && name.getNamespace().equals(Atmospheric.MOD_ID)) {
+		if (player.abilities.isCreativeMode && AtmosphericConfig.CLIENT.showUnobtainableDescription.get() && (name.getNamespace().equals(Atmospheric.MOD_ID) || name.getNamespace().equals("abnormals_delight"))) {
 			String id = name.getPath();
 			if (id.contains("travertine") || id.contains("aspen") || id.contains("kousa") || id.contains("grimwood") || id.contains("crustose"))
 				event.getToolTip().add(new TranslationTextComponent("tooltip.atmospheric.unobtainable").mergeStyle(TextFormatting.GRAY));
