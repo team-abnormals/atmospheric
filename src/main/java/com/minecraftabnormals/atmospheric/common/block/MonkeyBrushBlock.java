b85399e0fbeb2a12491952430bc91eb68ded803c
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-12-05 21:38:02 -0600

    Fixes #129

--- src/main/java/com/minecraftabnormals/atmospheric/common/block/MonkeyBrushBlock.java
+++ src/main/java/com/minecraftabnormals/atmospheric/common/block/MonkeyBrushBlock.java
@@ -35,7 +35,7 @@ public class MonkeyBrushBlock extends AbnormalsFlowerBlock implements IGrowable,
 	private static final Map<Direction, VoxelShape> SHAPES = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, Block.box(5.5D, 3.0D, 11.0D, 10.5D, 13.0D, 16.0D), Direction.SOUTH, Block.box(5.5D, 3.0D, 0.0D, 10.5D, 13.0D, 5.0D), Direction.WEST, Block.box(11.0D, 3.0D, 5.5D, 16.0D, 13.0D, 10.5D), Direction.EAST, Block.box(0.0D, 3.0D, 5.5D, 5.0D, 13.0D, 10.5D)));
 
 	public MonkeyBrushBlock(Properties properties) {
-		super(AtmosphericEffects.RELIEF::get, 120, properties);
+		super(AtmosphericEffects.RELIEF, 6, properties);
 		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP));
 	}
 
