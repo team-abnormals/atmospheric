ed6c644cbf94aaa81ef5c59b7e90609a8da95a13
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-08-01 12:49:53 -0500

    Fix Aloe Leaves being consumed twice; Closes #130

--- src/main/java/com/teamabnormals/atmospheric/common/item/AloeLeavesItem.java
+++ src/main/java/com/teamabnormals/atmospheric/common/item/AloeLeavesItem.java
@@ -2,44 +2,36 @@ package com.teamabnormals.atmospheric.common.item;
 
 import com.teamabnormals.atmospheric.core.other.AtmosphericCriteriaTriggers;
 import com.teamabnormals.atmospheric.core.other.AtmosphericDamageSources;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 import java.util.Random;
 
 public class AloeLeavesItem extends Item {
+
 	public AloeLeavesItem(Item.Properties properties) {
 		super(properties);
 	}
 
 	@Override
-	public ItemStack finishUsingItem(ItemStack stack, Level worldIn, LivingEntity entityLiving) {
-		super.finishUsingItem(stack, worldIn, entityLiving);
-		if (entityLiving instanceof ServerPlayer serverplayerentity) {
-			CriteriaTriggers.CONSUME_ITEM.trigger(serverplayerentity, stack);
-			serverplayerentity.awardStat(Stats.ITEM_USED.get(this));
-		}
-
-		Random rand = new Random();
-		if (rand.nextInt(5) == 0)
-			entityLiving.hurt(AtmosphericDamageSources.ALOE_LEAVES, 3.0F);
-		if (entityLiving.getRemainingFireTicks() > 0 && entityLiving instanceof ServerPlayer serverplayerentity) {
-			if (!entityLiving.getCommandSenderWorld().isClientSide()) {
-				AtmosphericCriteriaTriggers.PUT_OUT_FIRE.trigger(serverplayerentity);
+	public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+		ItemStack returnStack = super.finishUsingItem(stack, level, entity);
+		Random random = level.getRandom();
+		if (random.nextInt(5) == 0)
+			entity.hurt(AtmosphericDamageSources.ALOE_LEAVES, 3.0F);
+		if (entity.getRemainingFireTicks() > 0 && entity instanceof ServerPlayer serverPlayer) {
+			if (!entity.getCommandSenderWorld().isClientSide()) {
+				AtmosphericCriteriaTriggers.PUT_OUT_FIRE.trigger(serverPlayer);
 			}
 		}
-		entityLiving.clearFire();
-		return entityLiving.eat(worldIn, stack);
-	}
 
-	@Override
-	public UseAnim getUseAnimation(ItemStack stack) {
-		return UseAnim.EAT;
+		if (!level.isClientSide()) {
+			entity.clearFire();
+		}
+
+		return returnStack;
 	}
 }
