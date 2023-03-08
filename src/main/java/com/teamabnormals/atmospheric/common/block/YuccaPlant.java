5e44a7184e1e890ff35675b1b6b53f33e34ee919
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-08 14:27:18 -0600

    Barrel Cactus now prevent yucca damage

--- src/main/java/com/teamabnormals/atmospheric/common/block/YuccaPlant.java
+++ src/main/java/com/teamabnormals/atmospheric/common/block/YuccaPlant.java
@@ -1,11 +1,13 @@
 package com.teamabnormals.atmospheric.common.block;
 
+import com.teamabnormals.atmospheric.core.registry.AtmosphericItems;
 import com.teamabnormals.blueprint.common.advancement.EmptyTrigger;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.level.Level;
@@ -18,19 +20,22 @@ public interface YuccaPlant {
 
 	EmptyTrigger getCriteriaTrigger();
 
-	default void onYuccaCollision(BlockState state, Level worldIn, BlockPos pos, Entity entityIn) {
-		if (entityIn instanceof LivingEntity livingEntity && !(entityIn instanceof Bee)) {
-			if (!worldIn.isClientSide && (entityIn.xOld != entityIn.getX() || entityIn.zOld != entityIn.getZ())) {
-				double d0 = Math.abs(entityIn.getX() - entityIn.xOld);
-				double d1 = Math.abs(entityIn.getZ() - entityIn.zOld);
+	default void onYuccaCollision(BlockState state, Level level, BlockPos pos, Entity entity) {
+		if (entity instanceof LivingEntity livingEntity && !(entity instanceof Bee)) {
+			if (!level.isClientSide && (entity.xOld != entity.getX() || entity.zOld != entity.getZ())) {
+				double d0 = Math.abs(entity.getX() - entity.xOld);
+				double d1 = Math.abs(entity.getZ() - entity.zOld);
 				if (d0 >= (double) 0.003F || d1 >= (double) 0.003F) {
-					if (!entityIn.isCrouching()) {
-						livingEntity.knockback(this.getKnockbackForce(), -Mth.sin((float) (entityIn.getYRot() * Math.PI / 180.0F)), Mth.cos((float) (entityIn.getYRot() * Math.PI / 180.0F)));
+					if (!entity.isCrouching()) {
+						livingEntity.knockback(this.getKnockbackForce(), -Mth.sin((float) (entity.getYRot() * Math.PI / 180.0F)), Mth.cos((float) (entity.getYRot() * Math.PI / 180.0F)));
 					}
-					entityIn.hurt(this.getDamageSource(), 1.0F);
-					if (entityIn instanceof ServerPlayer serverplayerentity) {
-						if (!entityIn.getCommandSenderWorld().isClientSide() && !serverplayerentity.isCreative() && this.getCriteriaTrigger() != null) {
-							this.getCriteriaTrigger().trigger(serverplayerentity);
+					if (!livingEntity.getItemBySlot(EquipmentSlot.HEAD).is(AtmosphericItems.BARREL_CACTUS.get())) {
+						entity.hurt(this.getDamageSource(), 1.0F);
+					}
+
+					if (entity instanceof ServerPlayer serverPlayer) {
+						if (!entity.getCommandSenderWorld().isClientSide() && !serverPlayer.isCreative() && this.getCriteriaTrigger() != null) {
+							this.getCriteriaTrigger().trigger(serverPlayer);
 						}
 					}
 				}
