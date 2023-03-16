c0ffe4dcfdd82ff541fd9d03db35f3bbd43c6fd3
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-16 11:27:49 -0500

    Small rabbit type fix

--- src/main/java/com/teamabnormals/atmospheric/core/other/AtmosphericRabbitTypes.java
+++ src/main/java/com/teamabnormals/atmospheric/core/other/AtmosphericRabbitTypes.java
@@ -3,7 +3,9 @@ package com.teamabnormals.atmospheric.core.other;
 import com.teamabnormals.atmospheric.core.Atmospheric;
 import com.teamabnormals.blueprint.core.api.BlueprintRabbitTypes;
 import com.teamabnormals.blueprint.core.api.BlueprintRabbitTypes.BlueprintRabbitType;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
+@EventBusSubscriber(modid = Atmospheric.MOD_ID)
 public class AtmosphericRabbitTypes {
 	private static final int UNIQUE_OFFSET = 1337;
 
