23167f1017d762eeeabc6176bc17107ededff360
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-18 16:01:46 -0500

    Revert "Test"
    This reverts commit e6668550104e77f34d897425ccc127a82feea795.

--- src/main/java/com/teamabnormals/atmospheric/common/effect/PersistenceEffect.java
+++ src/main/java/com/teamabnormals/atmospheric/common/effect/PersistenceEffect.java
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.player.Player;
 import java.util.Map.Entry;
 
 public class PersistenceEffect extends MobEffect {
-
 	public PersistenceEffect() {
 		super(MobEffectCategory.BENEFICIAL, 15494786);
 	}
