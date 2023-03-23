835356f582efcbf097b182f3476531839e443b8a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-23 18:02:33 -0500

    Minor Passion Fruit Seed code tweaks

--- src/main/java/com/teamabnormals/atmospheric/common/entity/projectile/PassionFruitSeed.java
+++ src/main/java/com/teamabnormals/atmospheric/common/entity/projectile/PassionFruitSeed.java
@@ -3,64 +3,59 @@ package com.teamabnormals.atmospheric.common.entity.projectile;
 import com.teamabnormals.atmospheric.core.other.AtmosphericCriteriaTriggers;
 import com.teamabnormals.atmospheric.core.other.AtmosphericDamageSources;
 import com.teamabnormals.atmospheric.core.registry.AtmosphericEntityTypes;
-import net.minecraft.network.protocol.Packet;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrowableProjectile;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.network.PlayMessages;
 
 public class PassionFruitSeed extends ThrowableProjectile {
 	private int amplifier = 0;
 
-	public PassionFruitSeed(EntityType<? extends PassionFruitSeed> type, Level worldIn) {
-		super(type, worldIn);
+	public PassionFruitSeed(EntityType<? extends PassionFruitSeed> type, Level level) {
+		super(type, level);
 	}
 
-	public PassionFruitSeed(PlayMessages.SpawnEntity spawnEntity, Level world) {
-		this(AtmosphericEntityTypes.PASSION_FRUIT_SEED.get(), world);
+	public PassionFruitSeed(PlayMessages.SpawnEntity message, Level level) {
+		this(AtmosphericEntityTypes.PASSION_FRUIT_SEED.get(), level);
 	}
 
-	public PassionFruitSeed(Level worldIn, LivingEntity throwerIn, int amplifier) {
-		super(AtmosphericEntityTypes.PASSION_FRUIT_SEED.get(), throwerIn, worldIn);
-		this.amplifier = amplifier;
-	}
-
-	public PassionFruitSeed(Level worldIn, double x, double y, double z, int amplifier) {
-		super(AtmosphericEntityTypes.PASSION_FRUIT_SEED.get(), x, y, z, worldIn);
+	public PassionFruitSeed(Level level, LivingEntity shooter, int amplifier) {
+		super(AtmosphericEntityTypes.PASSION_FRUIT_SEED.get(), shooter, level);
 		this.amplifier = amplifier;
 	}
 
 	@Override
-	protected void onHit(HitResult result) {
-		super.onHit(result);
-		if (result.getType() == HitResult.Type.ENTITY) {
-			Entity entity = ((EntityHitResult) result).getEntity();
-			entity.hurt(AtmosphericDamageSources.causePassionFruitSeedDamage(this, this.getOwner()), 0.5F + amplifier);
-			if (this.getOwner() instanceof ServerPlayer serverplayerentity) {
-				if (!entity.getCommandSenderWorld().isClientSide()) {
-					AtmosphericCriteriaTriggers.SPIT_PASSION_FRUIT.trigger(serverplayerentity);
+	protected void onHitEntity(EntityHitResult result) {
+		super.onHitEntity(result);
+		Entity entity = this.getOwner();
+		if (entity instanceof LivingEntity living) {
+			result.getEntity().hurt(AtmosphericDamageSources.passionFruitSeedAttack(this, living).setProjectile(), 0.5F + amplifier);
+			if (living instanceof ServerPlayer serverPlayer) {
+				if (!serverPlayer.getCommandSenderWorld().isClientSide()) {
+					AtmosphericCriteriaTriggers.SPIT_PASSION_FRUIT.trigger(serverPlayer);
 				}
 			}
-		}
 
+			if (!level.isClientSide()) {
+				this.discard();
+			}
+		}
+	}
+
+	@Override
+	protected void onHitBlock(BlockHitResult result) {
+		super.onHitBlock(result);
 		if (!this.level.isClientSide) {
 			this.discard();
 		}
-
 	}
 
 	@Override
 	protected void defineSynchedData() {
-	}
-
-	@Override
-	public Packet<?> getAddEntityPacket() {
-		return NetworkHooks.getEntitySpawningPacket(this);
 	}
 }
