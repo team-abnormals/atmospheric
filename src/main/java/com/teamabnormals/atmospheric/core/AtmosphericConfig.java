b56ee1081d3ad9761087ca85988a2425f68a66b8
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-23 23:23:58 -0500

    Remove unused Client config

--- src/main/java/com/teamabnormals/atmospheric/core/AtmosphericConfig.java
+++ src/main/java/com/teamabnormals/atmospheric/core/AtmosphericConfig.java
@@ -23,29 +23,12 @@ public class AtmosphericConfig {
 		}
 	}
 
-	public static class Client {
-		public final ConfigValue<Boolean> showUnobtainableDescription;
-
-		public Client(ForgeConfigSpec.Builder builder) {
-			builder.push("misc");
-			this.showUnobtainableDescription = builder.comment("If unimplemented items should show that they are unobtainable in their item description").define("Show unobtainable description", true);
-			builder.pop();
-		}
-	}
-
 	public static final ForgeConfigSpec COMMON_SPEC;
 	public static final Common COMMON;
-
-	public static final ForgeConfigSpec CLIENT_SPEC;
-	public static final Client CLIENT;
 
 	static {
 		final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
 		COMMON_SPEC = specPair.getRight();
 		COMMON = specPair.getLeft();
-
-		Pair<Client, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(Client::new);
-		CLIENT_SPEC = clientSpecPair.getRight();
-		CLIENT = clientSpecPair.getLeft();
 	}
 }
