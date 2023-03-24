37a47362a88deae4326bc36861c36041dc14f10b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-23 23:00:37 -0500

    Fix Coarse Dirt not placing in Spiny Thicket

--- src/main/java/com/teamabnormals/atmospheric/common/levelgen/feature/CoarseDirtFeature.java
+++ src/main/java/com/teamabnormals/atmospheric/common/levelgen/feature/CoarseDirtFeature.java
@@ -1,6 +1,7 @@
 package com.teamabnormals.atmospheric.common.levelgen.feature;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.configurations.ProbabilityFeatureConfiguration;
@@ -14,5 +15,10 @@ public class CoarseDirtFeature extends TerrainPatchFeature {
 	@Override
 	public BlockState getTerrainState() {
 		return Blocks.COARSE_DIRT.defaultBlockState();
+	}
+
+	@Override
+	public boolean canReplace(BlockState state) {
+		return super.canReplace(state) || state.is(BlockTags.SAND);
 	}
 }
