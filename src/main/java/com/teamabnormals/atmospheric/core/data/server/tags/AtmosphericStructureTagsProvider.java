3da6e0e03114cf3ba693c45285b621b8f5cbade3
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-10-17 13:53:46 -0500

    Moved some code around

--- src/main/java/com/teamabnormals/atmospheric/core/data/server/tags/AtmosphericStructureTagsProvider.java
+++ src/main/java/com/teamabnormals/atmospheric/core/data/server/tags/AtmosphericStructureTagsProvider.java
@@ -1,18 +1,13 @@
 package com.teamabnormals.atmospheric.core.data.server.tags;
 
 import com.teamabnormals.atmospheric.core.Atmospheric;
-import net.minecraft.core.Registry;
+import com.teamabnormals.atmospheric.core.registry.AtmosphericStructures;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.StructureTagsProvider;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.StructureTags;
-import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class AtmosphericStructureTagsProvider extends StructureTagsProvider {
-
-	public static final ResourceKey<Structure> VILLAGE_SCRUBLAND = ResourceKey.create(Registry.STRUCTURE_REGISTRY, new ResourceLocation(Atmospheric.MOD_ID, "village_scrubland"));
 
 	public AtmosphericStructureTagsProvider(DataGenerator generator, ExistingFileHelper helper) {
 		super(generator, Atmospheric.MOD_ID, helper);
@@ -20,6 +15,6 @@ public class AtmosphericStructureTagsProvider extends StructureTagsProvider {
 
 	@Override
 	public void addTags() {
-		this.tag(StructureTags.VILLAGE).add(VILLAGE_SCRUBLAND);
+		this.tag(StructureTags.VILLAGE).add(AtmosphericStructures.VILLAGE_SCRUBLAND);
 	}
 }
---
+++ src/main/java/com/teamabnormals/atmospheric/core/registry/AtmosphericStructures.java
@@ -0,0 +1,16 @@
+package com.teamabnormals.atmospheric.core.registry;
+
+import com.teamabnormals.atmospheric.core.Atmospheric;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.Structure;
+
+public class AtmosphericStructures {
+	public static final ResourceKey<Structure> ARID_SHRINE = createKey("arid_shrine");
+	public static final ResourceKey<Structure> KOUSA_SANCTUM = createKey("kousa_sanctum");
+	public static final ResourceKey<Structure> VILLAGE_SCRUBLAND = createKey("village_scrubland");
+
+	private static ResourceKey<Structure> createKey(String id) {
+		return ResourceKey.create(Registry.STRUCTURE_REGISTRY, Atmospheric.location(id));
+	}
+}
