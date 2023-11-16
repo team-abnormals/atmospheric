cde7e997f961fdb8bcd04a3c7753d615e29329b7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-11-16 00:45:45 -0600

    Fix Yucca Bundles making dust particles

--- src/main/java/com/teamabnormals/atmospheric/common/block/YuccaBundleBlock.java
+++ src/main/java/com/teamabnormals/atmospheric/common/block/YuccaBundleBlock.java
@@ -4,9 +4,12 @@ import com.teamabnormals.atmospheric.core.registry.AtmosphericBlocks;
 import com.teamabnormals.blueprint.common.block.BlueprintFallingBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -29,5 +32,9 @@ public class YuccaBundleBlock extends BlueprintFallingBlock {
 	@Override
 	protected void falling(FallingBlockEntity fallingEntity) {
 		fallingEntity.setHurtsEntities(1.0F, 20);
+	}
+
+	@Override
+	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
 	}
 }
