d00b23e1908d2a258d673b6a336013530908cb87
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-03-03 01:56:13 -0600

    Fixes #222

--- src/main/java/com/teamabnormals/atmospheric/common/block/YuccaPlant.java
+++ src/main/java/com/teamabnormals/atmospheric/common/block/YuccaPlant.java
@@ -10,7 +10,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -20,17 +19,21 @@ public interface YuccaPlant {
 	DamageSource getDamageSource();
 
 	default void onYuccaCollision(BlockState state, Level level, BlockPos pos, Entity entity) {
-		if (entity instanceof LivingEntity livingEntity && !entity.getType().is(AtmosphericEntityTypeTags.YUCCA_IMMUNE)) {
+		if (entity instanceof LivingEntity living && !entity.getType().is(AtmosphericEntityTypeTags.YUCCA_IMMUNE)) {
 			if (!level.isClientSide && (entity.xOld != entity.getX() || entity.zOld != entity.getZ())) {
 				double d0 = Math.abs(entity.getX() - entity.xOld);
 				double d1 = Math.abs(entity.getZ() - entity.zOld);
 				if (d0 >= (double) 0.003F || d1 >= (double) 0.003F) {
 					if (!entity.isCrouching()) {
-						livingEntity.knockback(this.getKnockbackForce(), -Mth.sin((float) (entity.getYRot() * Math.PI / 180.0F)), Mth.cos((float) (entity.getYRot() * Math.PI / 180.0F)));
+						living.knockback(this.getKnockbackForce(), -Mth.sin((float) (entity.getYRot() * Math.PI / 180.0F)), Mth.cos((float) (entity.getYRot() * Math.PI / 180.0F)));
 					}
 
-					boolean wearingBarrelCactus = livingEntity.getItemBySlot(EquipmentSlot.HEAD).is(AtmosphericItems.BARREL_CACTUS.get());
-					entity.hurt(this.getDamageSource(), !wearingBarrelCactus ? 1.0F : 0.01F);
+					if (living.getItemBySlot(EquipmentSlot.HEAD).is(AtmosphericItems.BARREL_CACTUS.get())) {
+						entity.hurt(this.getDamageSource().bypassArmor(), 0.01F);
+					} else {
+						entity.hurt(this.getDamageSource(), 1.0F);
+					}
+
 					if (entity instanceof ServerPlayer serverPlayer) {
 						if (!entity.getCommandSenderWorld().isClientSide() && !serverPlayer.isCreative()) {
 							AtmosphericCriteriaTriggers.YUCCA_PRICK.trigger(serverPlayer);
