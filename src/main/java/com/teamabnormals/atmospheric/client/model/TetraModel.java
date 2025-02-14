4196f485e94a0f0c916e0dd5edf094f8c6105ee9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-02-13 20:10:07 -0600

    Rendering fixes

--- src/main/java/com/teamabnormals/atmospheric/client/model/TetraModel.java
+++ src/main/java/com/teamabnormals/atmospheric/client/model/TetraModel.java
@@ -1,7 +1,4 @@
-package com.teamabnormals.atmospheric.client.model;// Made with Blockbench 4.12.2
-// Exported for Minecraft version 1.17 or later with Mojang mappings
-// Paste this class into your mod and generate all required imports
-
+package com.teamabnormals.atmospheric.client.model;
 
 import com.teamabnormals.atmospheric.common.entity.Tetra;
 import net.minecraft.client.model.HierarchicalModel;
@@ -11,6 +8,7 @@ import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.Mth;
 
 public class TetraModel<T extends Tetra> extends HierarchicalModel<T> {
@@ -18,6 +16,7 @@ public class TetraModel<T extends Tetra> extends HierarchicalModel<T> {
 	private final ModelPart tail;
 
 	public TetraModel(ModelPart root) {
+		super(RenderType::entityTranslucent);
 		this.root = root;
 		this.tail = root.getChild("tail");
 	}
@@ -28,7 +27,7 @@ public class TetraModel<T extends Tetra> extends HierarchicalModel<T> {
 
 		PartDefinition body = part.addOrReplaceChild("body", CubeListBuilder.create().texOffs(2, 1).addBox(-0.5F, -3.0F, -3.0F, 1.0F, 2.0F, 5.0F), PartPose.offset(0.0F, 24.0F, 0.0F));
 		body.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(9, 12).addBox(-0.5F, -3.0F, -4.0F, 1.0F, 1.0F, 1.0F), PartPose.offset(0.0F, 0.0F, 0.0F));
-		part.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(3, 10).addBox(0.0F, -3.0F, 2.0F, 0.0F, 2.0F, 2.0F), PartPose.offset(0.0F, 24.0F, 0.0F));
+		part.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(3, 10).addBox(0.0F, -1.0F, 0.0F, 0.0F, 2.0F, 2.0F), PartPose.offset(0.0F, 22.0F, 2.0F));
 		part.addOrReplaceChild("right_fin", CubeListBuilder.create().texOffs(6, 9).addBox(-1.5F, -1.0F, 1.0F, 1.0F, 0.0F, 2.0F), PartPose.offset(0.0F, 24.0F, 0.0F));
 		part.addOrReplaceChild("left_fin", CubeListBuilder.create().texOffs(9, 9).addBox(0.5F, -1.0F, 1.0F, 1.0F, 0.0F, 2.0F), PartPose.offset(0.0F, 24.0F, 0.0F));
 		part.addOrReplaceChild("top_fin", CubeListBuilder.create().texOffs(3, 7).addBox(0.0F, -4.0F, -1.0F, 0.0F, 1.0F, 2.0F), PartPose.offset(0.0F, 24.0F, 0.0F));
