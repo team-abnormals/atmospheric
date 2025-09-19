da6aa5d9f2ea031aa87005d5ab3d2cba915c0204
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-09-18 19:15:37 -0500

    Closes #284

--- src/main/java/com/teamabnormals/atmospheric/core/mixin/BiomeMixin.java
+++ src/main/java/com/teamabnormals/atmospheric/core/mixin/BiomeMixin.java
@@ -3,24 +3,29 @@ package com.teamabnormals.atmospheric.core.mixin;
 import com.teamabnormals.atmospheric.core.registry.AtmosphericBlocks;
 import com.teamabnormals.blueprint.core.util.BlockUtil;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerLevel.class)
-public abstract class ServerLevelMixin {
+@Mixin(Biome.class)
+public abstract class BiomeMixin {
 
-	@Redirect(method = "tickPrecipitation", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/biome/Biome;shouldSnow(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)Z"))
-	private boolean shouldSnow(Biome biome, LevelReader level, BlockPos pos) {
-		if (!biome.warmEnoughToRain(pos)) {
+	@Shadow
+	public abstract boolean warmEnoughToRain(BlockPos pos);
+
+	@Inject(method = "shouldSnow", at = @At("HEAD"))
+	private void shouldSnow(LevelReader level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+		if (!this.warmEnoughToRain(pos)) {
 			if (pos.getY() >= level.getMinBuildHeight() && pos.getY() < level.getMaxBuildHeight() && level.getBrightness(LightLayer.BLOCK, pos) < 10) {
 				BlockState state = level.getBlockState(pos);
 				if (state.is(AtmosphericBlocks.BARREL_CACTUS.get())) {
@@ -32,13 +37,11 @@ public abstract class ServerLevelMixin {
 					BlockState belowState = level.getBlockState(pos.below());
 					Block newBlock = belowState.is(Blocks.CACTUS) ? AtmosphericBlocks.SNOWY_CACTUS.get() : belowState.is(AtmosphericBlocks.BARREL_CACTUS.get()) ? AtmosphericBlocks.SNOWY_BARREL_CACTUS.get() : null;
 
-					if (newBlock != null && level instanceof ServerLevel serverLevel) {
-						serverLevel.setBlockAndUpdate(pos.below(), BlockUtil.transferAllBlockStates(belowState, newBlock.defaultBlockState()));
+					if (newBlock != null && level instanceof WorldGenLevel worldgenLevel) {
+						worldgenLevel.setBlock(pos.below(), BlockUtil.transferAllBlockStates(belowState, newBlock.defaultBlockState()), 3);
 					}
 				}
 			}
 		}
-
-		return biome.shouldSnow(level, pos);
 	}
 }
--- src/main/java/com/teamabnormals/atmospheric/core/mixin/PistonBaseBlockMixin.java
+++ src/main/java/com/teamabnormals/atmospheric/core/mixin/PistonBaseBlockMixin.java
@@ -1,5 +1,7 @@
 package com.teamabnormals.atmospheric.core.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.teamabnormals.atmospheric.common.block.CurrantStalkBundleBlock;
 import com.teamabnormals.atmospheric.common.block.DragonRootsBlock;
 import com.teamabnormals.atmospheric.common.block.OrangeBlock;
@@ -7,22 +9,20 @@ import com.teamabnormals.atmospheric.core.registry.AtmosphericBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.piston.PistonBaseBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PistonBaseBlock.class)
 public abstract class PistonBaseBlockMixin {
 
-	@Redirect(method = "moveBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/piston/PistonBaseBlock;dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;)V"))
-	private void moveBlocks(BlockState state, LevelAccessor levelAccessor, BlockPos pos, BlockEntity entity) {
+	@WrapOperation(method = "moveBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/piston/PistonBaseBlock;dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;)V"))
+	private void moveBlocks(BlockState state, LevelAccessor levelAccessor, BlockPos pos, BlockEntity entity, Operation<Void> original) {
 		if (!(state.getBlock() instanceof OrangeBlock)) {
-			Block.dropResources(state, levelAccessor, pos, entity);
+			original.call(state, levelAccessor, pos, entity);
 		}
 		if (levelAccessor instanceof Level level) {
 			if (state.getBlock() instanceof CurrantStalkBundleBlock block) {
